6b9ea8b51c8b051f716d28804beedd00946b0b09
fnog98 <facu_009@live.com.ar>
2022-05-12 19:37:57 +0200

    come meter files su github

---
+++ hw-js1.js
@@ -0,0 +1,24 @@
+hw js
+meter archivos a github
+siempre pararme con git en la cd del repository
+
+
+git status /estatus dell repository 
+
+/files status lifecycle 
+
+git add nombrearchivo.js
+
+/posso fare git status di nuovo per veder
+/una volte che tute le file sono add posso fare il commit
+
+git comit -m 'nombrearchivo.js add'
+git log
+
+
+/hasta ahi todo en mi compu 
+/ahora cargo de git a github
+
+git push origin master 
+
+
